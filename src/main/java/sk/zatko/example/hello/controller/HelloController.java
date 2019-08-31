62e31307a6a3eb0e3bb68f53de5a1bd37bd89580
Jozef Zatko <jozefzatko.sk@gmail.com>
2019-08-31 11:33:17 +0200

    Added application.properties

---
+++ src/main/java/sk/zatko/example/hello/controller/HelloController.java
@@ -0,0 +1,17 @@
+package sk.zatko.example.hello.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sk.zatko.example.hello.service.HelloService;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private HelloService helloService;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return helloService.sayHello();
+    }
+}
--- src/main/java/sk/zatko/example/hello/service/HelloService.java
+++ src/main/java/sk/zatko/example/hello/service/HelloService.java
@@ -1,22 +1,25 @@
-package sk.zatko.example.hello;
+package sk.zatko.example.hello.service;
+
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @Slf4j
-@RestController
-public class HelloController {
+@Service
+public class HelloService {
 
-    @GetMapping("/")
+    @Value("${app.name}")
+    private String appName;
+
     public String sayHello() {
         log.info("Detecting local IP address...");
         try {
             String ipAddress = InetAddress.getLocalHost().getHostAddress();
             log.info("Current local IP address is {}", ipAddress);
-            return "Hello from: " + ipAddress;
+            return appName + " saying good afternoon from: " + ipAddress;
         } catch (UnknownHostException e) {
             log.error("Error while detecting IP address.", e);
             return "Error while detecting IP address.";
